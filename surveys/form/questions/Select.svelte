752db5d975130888a124a1b8e6ee54c616a2c8e9
Damian <sosnowski@outlook.com>
2023-05-22 16:52:45 +0100

    Fix styling in Select

--- surveys/form/questions/Select.svelte
+++ surveys/form/questions/Select.svelte
@@ -1,32 +1,32 @@
 <script lang="ts">
-    import { SelectQuestion, SurveyQuestion } from "shared/models/survey";
-    import { createEventDispatcher } from "svelte";
+	import { SelectQuestion, SurveyQuestion } from "shared/models/survey";
+	import { createEventDispatcher } from "svelte";
 
-    export let question: SelectQuestion;
+	export let question: SelectQuestion;
 
-    const dispatch = createEventDispatcher<{
-        submit: string;
-    }>();
+	const dispatch = createEventDispatcher<{
+		submit: string;
+	}>();
 
-    const onSubmit = (e: SubmitEvent) => {
-        const data = new FormData(e.target as HTMLFormElement);
+	const onSubmit = (e: SubmitEvent) => {
+		const data = new FormData(e.target as HTMLFormElement);
 
-        dispatch("submit", data.get("response") as string);
-    };
+		dispatch("submit", data.get("response") as string);
+	};
 </script>
 
 <div class="flex flex-col gap-4 w-full h-full justify-start items-center">
-    <p class="text-center text-slate-800 text-lg">{question.label}</p>
+	<p class="text-center text-slate-800 text-lg">{question.label}</p>
 
-    <form class="contents" on:submit|preventDefault={onSubmit}>
-        <select
-            class="w-full rounded-md border-0 py-1.5 text-slate-800 shadow-sm ring-1 ring-inset ring-slate-200 placeholder:text-slate-400 focus:ring-2 focus:ring-inset focus:ring-fuchsia-600 leading-6"
-            name="response"
-        >
-            <option value="" disabled selected>Select an option</option>
-            {#each question.options as option}
-                <option value={option.value}>{option.label}</option>
-            {/each}
-        </select>
-    </form>
+	<form class="contents" on:submit|preventDefault={onSubmit}>
+		<select
+			class="w-full rounded-md border-0 p-2 text-slate-800 shadow-sm ring-1 ring-inset ring-slate-200 placeholder:text-slate-400 focus:ring-2 focus:ring-inset focus:ring-fuchsia-600 leading-6"
+			name="response"
+		>
+			<option value="" disabled selected>Select an option</option>
+			{#each question.options as option}
+				<option value={option.value}>{option.label}</option>
+			{/each}
+		</select>
+	</form>
 </div>
